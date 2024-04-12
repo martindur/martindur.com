247d1569b8179a48391bd4dc55a28ed6674555d6
Martin Durhuus <martindurhuus@gmail.com>
2024-04-12 14:21:01 +0200

    feat(landing): updated landing page with 'track record'

--- lib/martindur_web/controllers/page_controller.ex
+++ lib/martindur_web/controllers/page_controller.ex
@@ -7,13 +7,50 @@ defmodule MartindurWeb.PageController do
 
     projects = [
       %{
-        name: "Foo"
+        name: "ai image generator",
+        description: "freepik figma plugin",
+        points: [
+          "full-stack development",
+          "ux design",
+          "user testing"
+        ],
+        img: ~p"/images/plugin_ai-image-generator.png",
+        url:
+          "https://www.figma.com/community/plugin/1253173167259963280/ai-image-generator-by-freepik"
       },
       %{
-        name: "Bar"
+        name: "flaticon",
+        description: "flaticon figma plugin",
+        points: [
+          "front-end development",
+          "ux design",
+          "user testing"
+        ],
+        img: ~p"/images/plugin_flaticon.png",
+        url: "https://www.figma.com/community/plugin/1183020129233350982/flaticon-by-freepik"
       },
       %{
-        name: "Baz"
+        name: "iconfinder",
+        description: "web marketplace for icons & illustrations",
+        points: [
+          "full-stack development",
+          "rest api",
+          "infrastructure"
+        ],
+        img: ~p"/images/iconfinder.png",
+        url: "https://iconfinder.com"
+      },
+      %{
+        name: "plant jammer",
+        description: "web app for generating recipes",
+        points: [
+          "back-end development",
+          "rest api",
+          "graphql api",
+          "infrastructure"
+        ],
+        img: ~p"/images/plantjammer.png",
+        url: "https://plantjammer.com"
       }
     ]
 
